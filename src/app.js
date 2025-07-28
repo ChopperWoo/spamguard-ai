76843bdf4c366dc0fc51fc054e38e0adc3ff7b04
George Woodman <george.woodman@hotmail.com>
2025-07-28 17:17:46 +0100

    Initial SpamGuard AI application

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpamGuardAI from './components/SpamGuardAI';
+
+function App() {
+  return (
+    <div className="App">
+      <SpamGuardAI />
+    </div>
+  );
+}
+
+export default App;
