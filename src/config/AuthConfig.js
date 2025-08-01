d7ecd71d2ceb2fb21f32c77a4fa04a45517df6f0
George Woodman <george.woodman@hotmail.com>
2025-08-01 14:01:33 +0100

    authfig.js updated

--- src/config/AuthConfig.js
+++ src/config/AuthConfig.js
@@ -1,9 +1,9 @@
 export const msalConfig = {
   auth: {
-    clientId: "your-client-id-from-azure",
+    clientId: "fa4735b2-0249-41a1-a288-bc7c731a24cc",
     authority: "https://login.microsoftonline.com/common",
     redirectUri: process.env.NODE_ENV === 'production' 
-      ? "https://your-app-name.netlify.app" 
+      ? "https://spamguard-ai.netlify.app" 
       : "http://localhost:3000",
   },
   cache: {
